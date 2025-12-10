6d0cc00f89339f6b5a4a709f4f05124117b8bcad
Yatharth <yatharthchouhan22@gmail.com>
2025-12-10 23:18:31 +0530

    Average testcase for homepage

---
+++ tests/test_homepage.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_homepage_best_case(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_homepage_average_case(client):
+    response = client.get("/?ref=test")
+    assert response.status_code == 200
